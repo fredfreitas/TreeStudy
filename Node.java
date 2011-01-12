fc64adf6c0890f11208b6c7b1c5787f9039428df
fredfreitas <fredaraujofreitas@gmail.com>
2011-01-12 00:42:31 -0300

    Node class ok

---
+++ Node.java
@@ -0,0 +1,14 @@
+public class Node {
+	public int key;
+	public Node left, right;
+
+	public Node() {
+		key = 0;
+		left = right = null;
+	}
+
+	public Node(int k) {
+		key = k;
+		left = right = null;
+	}
+}
